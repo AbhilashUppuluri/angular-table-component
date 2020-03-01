6d0ede786f0ee1c2b6d948a928bf1cca15a8b70d
AbhilashUppuluri <abhilashuppuluri.7@gmail.com>
2020-03-01 16:58:45 +0530

    Add test cases for table service

--- src/app/table.service.spec.ts
+++ src/app/table.service.spec.ts
@@ -1,16 +1,41 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from  '@angular/common/http/testing';
 
 import { TableService } from './table.service';
 
 describe('TableService', () => {
   let service: TableService;
-
+  let httpMock: HttpTestingController;
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [TableService]
+    });
     service = TestBed.inject(TableService);
+    httpMock = TestBed.get(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should be able to retreive data from JSON', () => {
+    const dummyData = [{},{}];
+    service.getTableData().subscribe((data:any) => {
+      expect(data.length).toBe(2);
+    });
+    const mockRequest = httpMock.expectOne(`./assets/sample_data.json`);
+    expect(mockRequest.request.method).toBe('GET');
+    mockRequest.flush(dummyData);
+    httpMock.verify();
+  });
+
+  it('should be able submit rowId, status API', () => {
+    service.submitRow(1, "read").subscribe((data:any) => {
+     
+    });
+    const mockRequest = httpMock.expectOne(`/api/submit`);
+    expect(mockRequest.request.method).toBe('POST');
+    httpMock.verify();
   });
 });
